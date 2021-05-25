1054061fe25cde8f3c36c20938f3b319fb5987e6
alberto006 <alberto.soriano864@gmail.com>
2021-05-25 11:54:06 -0600

    update_2405_1154

--- src/app/components/admin/usuarios/usuario.service.ts
+++ src/app/components/admin/usuarios/usuario.service.ts
@@ -18,9 +18,55 @@ export class UsuarioService {
   //------------------------------------------------------------------------------------------------------------//
   getUsuarios(): Observable<any>{
     return this.http.get<any>(`${this.apiUrl}/om/usuarios`)
+    .pipe(
+      tap(),
+      catchError(this.handleError<any>('Error al obtener la lista de usuarios'))
+    )
   }
   //------------------------------------------------------------------------------------------------------------//
 
+  //------------------------------------------------------------------------------------------------------------//
+  getRolesUsuarios(): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/om/usuarios/roles`)
+      .pipe(
+        tap(),
+        catchError(this.handleError('Error al obtener la lista de roles de usuarios'))
+      )
+  }
+  //------------------------------------------------------------------------------------------------------------//
+
+  //------------------------------------------------------------------------------------------------------------//
+  postUsuario(usuario:string,correo:string,rolID:number,password:string): Observable<any>{
+    return this.http.post<any>(`${this.apiUrl}/om/usuarios/crear`,{usuario,correo,rolID,password})
+      .pipe(
+        tap(),
+        catchError(this.handleError<any>('Error al crear el usuario'))
+      )
+  }
+
+  //------------------------------------------------------------------------------------------------------------//
+
+  //------------------------------------------------------------------------------------------------------------//
+  // FUNCTION PARA MOSTRAR MENSAJES EN PANTALLA AL USUARIO
+  notificacion(mensaje:string){
+    this._snackBar.open(mensaje, 'Cerrar',{
+      duration:5000,
+      horizontalPosition:'end',
+      verticalPosition:'top'
+    })
+  }
+  //------------------------------------------------------------------------------------------------------------//
+
+  //------------------------------------------------------------------------------------------------------------//
+  // FUNCTION PARA MANEJO DE ERRORES
+  private handleError<T>(operation = 'operation',result?: T){
+    return (error:any): Observable<T>=>{
+      console.log('error en la aplicacion: '+JSON.stringify(error));
+      this.notificacion('Error en la aplicacion: '+operation)
+     return of(result as T);
+    }
+  }
+  //------------------------------------------------------------------------------------------------------------//
 
 
 }
